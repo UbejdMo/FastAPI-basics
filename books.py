14dc241dbf0547f7cc1eac7363b929a37de19a44
Ubejd Morina <morinaubejd@gmail.com>
2026-04-28 14:50:44 +0200

    create get method

--- books.py
+++ books.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-async def first_api():
-    return{"message":"Hello Ubejd!"}
+
+BOOKS = [
+    {'title': 'Title One','author':'author One','category':'science'},
+    {'title': 'Title Two','author':'author Two','category':'science'},
+    {'title': 'Title Three','author':'author Three','category':'history'},
+    {'title': 'Title Four','author':'author Four','category':'math'},
+    {'title': 'Title Five','author':'author Five','category':'math'},
+    {'title': 'Title Six','author':'author Two','category':'math'}
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
