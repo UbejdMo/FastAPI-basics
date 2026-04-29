b5a28e4bcb567e05ec1ec87daf21de3fe69630c3
Ubejd Morina <morinaubejd@gmail.com>
2026-04-29 12:27:13 +0200

    add Book request validation

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
 from fastapi import Body,FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,6 +9,7 @@ class Book:
     author:str
     description:str
     rating:int
+
     def __init__(self,id,title,author,description,rating):
         self.id = id
         self.title = title
@@ -15,6 +17,15 @@ class Book:
         self.description = description
         self.rating = rating
     
+class BookRequest(BaseModel):
+    id:int
+    title:str
+    author:str
+    description:str
+    rating:int
+
+
+
 BOOKS = [
     Book(1,'Computer Science Pro','codingwithubi','A very nice book',5),
     Book(2,'Be fast with FastApi','codingwithubi','A great book!',5),
@@ -29,5 +40,7 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request:BookRequest):
+    new_book = Book(**book_request.model_dump())
+    print(type(new_book))
+    BOOKS.append(new_book)
